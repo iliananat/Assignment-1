007168906d3bb42560901f47cbdabc477397eb70
Iliana Natsiou <iliananat7@gmail.com>
2023-12-12 15:51:38 +0200

    Initial repository commit

---
+++ Game-Bar-master/src/games/ImageLoader.java
@@ -0,0 +1,28 @@
+package games;
+
+/**
+ *
+ * @author Yashraj Balidani
+ * @author Milind Jain
+ * @version 1.0
+ * 
+ */
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class ImageLoader {
+
+    public static BufferedImage loadImage(String path) {
+        try {
+            return ImageIO.read(new File("data" + path));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return null;
+
+    }
+}
---
+++ Game-Bar-master/src/games/Snake.java
@@ -0,0 +1,33 @@
+/**
+ *  set this file visible in code to start snake game
+ *  snake.setVisible(true);
+ *  
+ *  snake play is implementation of game file this initialize the game;
+ */
+package games;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Yashraj Balidani
+ * @author Milind Jain
+ * @version 1.0
+ * 
+ */
+public class Snake 
+{
+    public static void main(String[] args)
+    {
+        JFrame obj = new JFrame("Snake Xenzia");
+        snakeplay splay = new snakeplay();
+        obj.setBounds(10, 10, 905, 700);
+        obj.setBackground(Color.darkGray);
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(splay);
+        
+    }
+}
